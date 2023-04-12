c5eca1b2ea6e7d07c359886f00f186904f7f5e4a
manasa <manasa@gmail.com>
2023-04-12 09:58:34 +0000

    commiting toIPL Dashboard

---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {details} = props
+  const {id, name, teamImageUrl} = details
+  return (
+    <li key={id} className="team-list-container">
+      <Link className="link" to={`/team-matches/${id}`}>
+        <div>
+          <img src={teamImageUrl} alt={name} className="team-logo" />
+          <p className="team-name">{name}</p>
+        </div>
+      </Link>
+    </li>
+  )
+}
+
+export default TeamCard
